b04cd046f60e157e134bcc5a76041efb5dd1710c
kayt.barrett <kayt.barrett22@gmail.com>
2024-10-28 14:25:59 -0600

    Defined File class and it's methods in File  header and source files

---
+++ File.cpp
@@ -0,0 +1,45 @@
+#include "File.h"
+#include <string>
+#include <ctime>
+#include <iostream>
+#include <sstream>
+
+File::File(const std::string& name){
+    this->name = name;
+    this->content = "";
+    this->size = content.size();
+    this->createdAt = std::time(nullptr);
+    this->modifiedAt = createdAt;
+}
+
+void File::write(std::string& data){
+    content = data;
+    size = content.size();
+    modifiedAt = std::time(nullptr);
+}
+
+std::string File::read() const{
+    return content;
+}
+
+void File::update(std::string& data){
+    write(data);
+}
+
+std::string File::getName() const {
+    return name;
+}
+
+size_t File::getSize() const {
+    return size;
+}
+
+std::string File::getMetadata() const {
+    std::stringstream metadata;
+    metadata << "File name: " << name << "\n"
+             << "File size: " << size << " bytes\n"
+             << "Created at: " << std::asctime(std::localtime(&createdAt)) << "\n"
+             << "Modified last: " << std::asctime(std::localtime(&modifiedAt));
+
+    return metadata.str();
+}
--- File.h
+++ File.h
@@ -1,6 +1,31 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <string>
+#include <ctime>
+
+
+class File {
+
+private:
+    std::string name;
+    std::string content;
+    size_t size;
+    std::time_t createdAt;
+    std::time_t modifiedAt;
+
+public:
+    File(const std::string& name);
+    void write(std::string& data);
+    std::string read() const;
+    void update(std::string& data);
+
+    std::string getName() const;
+    size_t getSize() const;
+    std::string getMetadata() const;
+
+    
+};
 
 
 #endif
