350f06388367fd3e1f357390379442c704c3b792
kayt.barrett <kayt.barrett22@gmail.com>
2024-10-28 15:06:02 -0600

    Declared Directory class and its methods in Directory.h

--- Directory.h
+++ Directory.h
@@ -1,6 +1,35 @@
 #ifndef DIRECTORY_H
 #define DIRECTORY_H
 
+#include "File.h"
+#include <vector>
+#include <string>
+#include <memory>
+
+class Directory {
+
+private:
+    std::string name;
+    std::vector<std::unique_ptr<File>> files;
+    std::vector<std::unique_ptr<Directory>> subdirectories;
+    Directory* parent;
+
+public:
+    Directory(const std::string& name, Directory* parent = nullptr);
+    
+    void createFile(const std::string& fileName);
+    void deleteFile(const std::string& fileName);
+    File* getFile(const std::string& fileName);
+
+    void createDirectory(const std::string& dirName);
+    void deleteDirectory(const std::string& dirName);
+    Directory* getDirectory(const std::string& dirName);
+    void listContents() const;
+    std::string getName() const;
+
+    Directory* getParent() const;
+
+};
 
 
 #endif
